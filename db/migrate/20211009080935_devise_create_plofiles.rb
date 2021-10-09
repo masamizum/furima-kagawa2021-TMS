32bd7349ff7516e1dc37760740448d00daa46f96
masamizum <sky.masahiro@gmail.com>
2021-10-09 17:29:31 +0900

    plofileモデル作成とplofilesテーブルの記述

--- db/migrate/20211009080935_devise_create_plofiles.rb
+++ db/migrate/20211009080935_devise_create_plofiles.rb
@@ -4,8 +4,11 @@ class DeviseCreatePlofiles < ActiveRecord::Migration[6.0]
   def change
     create_table :plofiles do |t|
       ## Database authenticatable
-      t.string :email,              null: false, default: ""
-      t.string :encrypted_password, null: false, default: ""
+      t.string :last_name,          null: false
+      t.string :first_name,         null: false
+      t.string :last_name_kana,     null: false
+      t.string :first_name_kana,    null: false
+      t.datetime :birth_date,       null: false
 
       ## Recoverable
       t.string   :reset_password_token
