ab8aa534cf6437735abf3c18e8671e471b8e6a4b
masamizum <sky.masahiro@gmail.com>
2021-10-09 19:41:38 +0900

    Revert "plofileモデル作成とplofilesテーブルの記述"
    This reverts commit 32bd7349ff7516e1dc37760740448d00daa46f96.

--- db/migrate/20211009080935_devise_create_plofiles.rb
+++ db/migrate/20211009080935_devise_create_plofiles.rb
@@ -4,11 +4,8 @@ class DeviseCreatePlofiles < ActiveRecord::Migration[6.0]
   def change
     create_table :plofiles do |t|
       ## Database authenticatable
-      t.string :last_name,          null: false
-      t.string :first_name,         null: false
-      t.string :last_name_kana,     null: false
-      t.string :first_name_kana,    null: false
-      t.datetime :birth_date,       null: false
+      t.string :email,              null: false, default: ""
+      t.string :encrypted_password, null: false, default: ""
 
       ## Recoverable
       t.string   :reset_password_token
