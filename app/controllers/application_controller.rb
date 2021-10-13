8e8e51460b0caa2fab37fd1c3f4881ab6b6d02e4
masamizum <sky.masahiro@gmail.com>
2021-10-14 00:37:11 +0900

    ログインの有無で表示が変わる（作業中）

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
   before_action :basic_auth
-
+  
   private
 
   def basic_auth
@@ -10,4 +10,9 @@ class ApplicationController < ActionController::Base
       end
     end
   end
+  
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :last_name, :first_name, :last_name_kana, :first_name_kana, :birth_date])
+  end
+
 end
