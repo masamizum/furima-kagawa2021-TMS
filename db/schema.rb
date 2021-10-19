9e7d2a6bac30534e4451761395b81cf868d2a606
galakuma <yumarin7@mx81.tiki.ne.jp>
2021-10-19 20:03:48 +0900

    商品一覧表示に関するviewファイルの実装

--- db/schema.rb
+++ db/schema.rb
@@ -49,7 +49,7 @@ ActiveRecord::Schema.define(version: 2021_10_17_103718) do
   end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.string "nickname"
+    t.string "nickname", null: false
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "last_name", null: false
